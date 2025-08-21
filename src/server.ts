416119f09c24c05fd50101c809927da72f2e9939
Mady-Lun <mady.lun22@kit.edu.kh>
2025-08-21 21:23:47 +0700

    CORS added

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
 import express from 'express';
+import cors from 'cors';
 
 // custom modules
 import config from './config';
 
+//types
+import type { CorsOptions } from 'cors';
+
 const app = express();
+
+//configure CORS options
+const corsOptions: CorsOptions = {
+  origin(origin, callback) {
+    if (config.NODE_ENV === 'development' || !origin || config.WHITELIST_ORIGINS.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error(`CORS error: ${origin} is not allowed`), false);
+    }
+    console.log(`CORS error: ${origin} is not allowed`);
+  }
+};
+
+//apply cors middleware
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.json({ 
